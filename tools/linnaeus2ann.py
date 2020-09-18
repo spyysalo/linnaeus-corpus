b047647016be58a444e04fa2af5e7efb849b638b
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2020-09-18 14:54:45 +0300

    python3

--- tools/linnaeus2ann.py
+++ tools/linnaeus2ann.py
@@ -7,14 +7,15 @@ from os import path
 
 from linnaeus import load_linnaeus
 
+
 def main(argv):
     if len(argv) != 4:
-        print >> sys.stderr, 'Usage: linnaeus2ann ANNFILE TXTDIR OUTDIR'
+        print('Usage: linnaeus2ann ANNFILE TXTDIR OUTDIR', file=sys.stderr)
         return 1
     annfn, txtdir, outdir = argv[1:]
 
     if not path.isdir(outdir):
-        print >> sys.stderr, '%s is not a directory' % outdir
+        print('%s is not a directory' % outdir, file=sys.stderr)
         return 1
 
     documents = load_linnaeus(annfn, txtdir)
@@ -26,6 +27,7 @@ def main(argv):
         annout = path.join(outdir, d.id+'.ann')
         with io.open(annout, 'wt', encoding='utf-8') as out:
             out.write(u'\n'.join(d.to_standoff()))
-            
+
+
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
